cb8912744e392e659deeb3beb5e3de0695bcbe1f
Topaz712 <tzam712@gmail.com>
2024-02-05 00:59:09 -0600

    REFACTORED: posts & users_controller to use before_action method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def create
     user = User.new(user_params)
     if user.save
@@ -13,33 +15,23 @@ class UserController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
-
-    if user 
-      render json: user, status: :ok 
-    else 
-      render json: {message: 'not found'}, status: :not_found
-    end
+    render json: @user, status: :ok
   end
 
   def update
-    user = User.find(params[:id])
-
-    if user.update(user_params)
-      render json: user, satus: :ok 
+    if @user.update(user_params)
+      render json: @user, status: :ok 
     else 
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-
-    if user.destroy
+    if @user.destroy
       # return a response with only headers and no body
       head :no_content
     else 
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
@@ -47,5 +39,9 @@ class UserController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
