4d0ca11de4125bb2e9ef2645f39f08f65d8cca51
Topaz712 <tzam712@gmail.com>
2024-02-05 00:38:49 -0600

    ADDED: user model & controller for testing w/ 'Faker gem'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/create'
+  get 'user/show'
+  get 'user/update'
+  get 'user/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :posts # will generate all routes for posts including POST /posts with action create
-  
+  resources :posts
+  resources :users
+  # will generate all routes for posts including POST /posts with action create
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
